4ce9b198aaf48e8f7ee53b2a2a9ce914ee56b60d
jeter <fl526128@gmail.com>
2021-10-11 14:59:27 +0800

    預約路徑完成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  devise_for :users do
+  devise_for :users 
+  devise_scope :users do
+    resources :users do
+      collection do
+        get 'reserve', to: 'users#reserve'
+      end
+    end
   end
 end
