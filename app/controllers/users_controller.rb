4ef59c8cb86e1b38d7ea1320293e7424fd19f1b8
jeter <fl526128@gmail.com>
2021-10-15 00:14:14 +0800

    預約訂單完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,2 @@
 class UsersController < ApplicationController
-  def reserve
-	end
-
-  def update
-    @user = current_user
-    if @user.update(user_params)
-      redirect_to root_path ,notice: "預約成功"
-    else
-      render :reserve
-    end
-  end
-
-  private
-    def user_params
-      params.require(:user).permit(:name, :email, :telephone, :address, :cellphone)
-    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_145854) do
+ActiveRecord::Schema.define(version: 2021_10_14_160446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "reserves", force: :cascade do |t|
-    t.string "ame"
     t.string "email"
     t.string "address"
     t.string "cellphone"
@@ -24,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_10_14_145854) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "appointment"
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
