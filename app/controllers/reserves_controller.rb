c860176fc3324b5b0252faeb07fd2f4882a4218a
jeter <fl526128@gmail.com>
2021-10-16 00:59:31 +0800

    解決resef問題

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -4,16 +4,16 @@ class ReservesController < ApplicationController
   end
 
   def create
-    @reserve = Reserve.new(reserve_params)
+    @reserve = current_user.reserves.new(reserve_params)
     if @reserve.save
       redirect_to root_path, notice:'預約成功'
     else
-      render :reserve
+      redirect_to root_path, notice:'失敗'
     end
   end
 
   private
     def reserve_params
-      params.require(:reserve).permit(:name, :email, :telephone, :address, :cellphone)
+      params.require(:reserve).permit(:name, :email, :telephone, :address, :cellphone, :appointment)
     end
 end
