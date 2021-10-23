8c71b15879ab0792fb608434465a05161608e241
jeter <fl526128@gmail.com>
2021-10-23 20:18:58 +0800

    新增figaro套件和三方登入套件

--- Gemfile
+++ Gemfile
@@ -23,6 +23,11 @@ gem 'sidekiq-scheduler', '~> 3.1'
 gem 'redis', '~> 4.0'
 
 gem "rails-i18n"
+
+gem 'omniauth'
+gem 'omniauth-facebook'
+gem 'omniauth-google-oauth2'
+gem 'figaro'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_create :generate_authentication_token
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :reserves       
+  has_many :reserves
+  
+  def generate_authentication_token
+    self.authentication_token = Devise.friendly_token
+  end
 end
